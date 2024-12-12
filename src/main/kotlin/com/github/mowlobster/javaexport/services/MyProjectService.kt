284f5d9ed66036b680fb5e49601d94f3321091fd
GitHub Action <action@github.com>
2024-12-12 07:20:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/mowlobster/javaexport/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mowlobster/javaexport/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.mowlobster.javaexport.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mowlobster.javaexport.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
